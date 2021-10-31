aae5c32a59845a20f1c2649662af778e061d2859
abdulkadir <redstone239@icloud.com>
2021-10-31 14:28:51 +0300

    sayı tahmin oyunu ver 1.0

---
+++ app/src/main/java/com/example/calisma2_zaroyunu/OyunAnaSayfa.java
@@ -0,0 +1,68 @@
+package com.example.calisma2_zaroyunu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Scanner;
+
+public class OyunAnaSayfa extends AppCompatActivity {
+    public Button buttonTahmin;
+    public TextView kalanHak;
+    public TextView yonlendir;
+    public EditText tahmin;
+    public  int gelenTahmin;
+    public  int randomSayi;
+    public int sayac = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oyun_ana_sayfa);
+
+        randomSayi = (int)(Math.random()*101);
+        buttonTahmin = findViewById(R.id.button2);
+        kalanHak = findViewById(R.id.textView3);
+        kalanHak.setText("Kalan Hak: 5 (Sayı:" + randomSayi + ")");
+        yonlendir = findViewById(R.id.textView4);
+        tahmin = findViewById(R.id.editTextTextPersonName);
+
+        buttonTahmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sayac -= 1;
+                gelenTahmin = Integer.parseInt(tahmin.getText().toString());
+
+                if( gelenTahmin < randomSayi)
+                    yonlendir.setText("ARTTIR");
+
+                else if(gelenTahmin > randomSayi)
+                    yonlendir.setText("AZALT");
+                else {
+                    Intent gecis = new Intent(getApplicationContext(), OyunSonuc.class);
+                    gecis.putExtra("sonuc",true);
+                    startActivity(gecis);
+                    return;
+                }
+
+                if (sayac == 0) {
+                    Intent gecis = new Intent(getApplicationContext(), OyunSonuc.class);
+                    gecis.putExtra("sonuc",false);
+                    gecis.putExtra("sayi",randomSayi);
+                    startActivity(gecis);
+                    return;
+                }
+
+                kalanHak.setText("Kalan Hak: "+ sayac);
+                tahmin.setText("");
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/calisma2_zaroyunu/OyunSonuc.java
@@ -0,0 +1,49 @@
+package com.example.calisma2_zaroyunu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class OyunSonuc extends AppCompatActivity {
+    public Button tekrarOyna;
+    public ImageView sonucSimge;
+    public TextView sonucAciklama;
+    public boolean gelenSonuc;
+    public int gelenSayi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oyun_sonuc);
+
+        tekrarOyna = findViewById(R.id.button3);
+        sonucSimge = findViewById(R.id.imageView2);
+        sonucAciklama = findViewById(R.id.textView2);
+
+        gelenSonuc = getIntent().getBooleanExtra("sonuc", false);
+        gelenSayi = getIntent().getIntExtra("sayi", 0);
+
+
+        if (gelenSonuc){
+            sonucSimge.setImageResource(R.drawable.smiley_face);
+            sonucAciklama.setText("KAZANDINIZ");
+        }
+        else{
+            sonucSimge.setImageResource(R.drawable.sad_face);
+            sonucAciklama.setText("KAYBETTİNİZ\n Bulamadığınız Sayı: " + gelenSayi);
+        }
+
+        tekrarOyna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gecis = new Intent(getApplicationContext(),OyunAnaSayfa.class);
+                startActivity(gecis);
+            }
+        });
+
+    }
+}
